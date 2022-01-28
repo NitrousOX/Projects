002244eee526f8489d2cfea3fa07e61cedd89fc1
NitrousOX <47796111+NitrousOX@users.noreply.github.com>
2022-01-28 16:05:35 +0100

    Create A5java.js

---
+++ A5java.js
@@ -0,0 +1,34 @@
+function Madjarska_inf()
+{
+    mywindow = window.open("https://localhost:44331/Sajtovi/Madjarska.html", "mywindow", "width=200,height=500");
+}
+
+function Hrvatska_inf() {
+    mywindow = window.open("https://localhost:44331/Sajtovi/Hrvatska.html", "mywindow", "width=200,height=500");
+}
+
+function Bosna_inf() {
+    mywindow = window.open("https://localhost:44331/Sajtovi/Bosna.html", "mywindow", "width=200,height=500");
+}
+function Crna_Gora_inf() {
+    mywindow = window.open("https://localhost:44331/Sajtovi/Crna%20Gora.html", "mywindow", "width=200,height=500");
+}
+
+function Albanija_inf() {
+    mywindow = window.open("https://localhost:44331/Sajtovi/Albanija.html", "mywindow", "width=200,height=500");
+}
+
+function Makedonija_inf() {
+    mywindow = window.open("https://localhost:44331/Sajtovi/Makedonija.html", "mywindow", "width=200,height=500");
+}
+
+function Bugarska_inf() {
+    mywindow = window.open("https://localhost:44331/Sajtovi/Bugarska.html", "mywindow", "width=200,height=500");
+}
+function Rumunija_inf() {
+    mywindow = window.open("https://localhost:44331/Sajtovi/Rumunija.html", "mywindow", "width=200,height=500");
+}
+
+function Srbija_inf() {
+    mywindow = window.open("https://localhost:44331/Sajtovi/Srbija.html", "mywindow", "width=200,height=500");
+}
